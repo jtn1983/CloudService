8fae7a2714bab18cf9dc9f5dd8ea6c8071d00335
Yakov Tenilin <yakov@tenilin.ru>
2022-06-01 15:54:38 +0300

    Final

--- src/main/java/ru/tenilin/cloudservice/CloudServiceApplication.java
+++ src/main/java/ru/tenilin/cloudservice/CloudServiceApplication.java
@@ -3,10 +3,20 @@ package ru.tenilin.cloudservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class CloudServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src/main/resources/cloud");
+        if (!Files.exists(path)) {
+            Files.createDirectory(path);
+        }
         SpringApplication.run(CloudServiceApplication.class, args);
     }
 
